f8094e8b30084f89f678a20f07e6d1e37204bd79
Allassane <dickoallassane1997@gmail.com>
2022-09-22 19:08:48 +0000

    test branch

--- RencontreAfricaine/app/src/main/java/com/lnd/RencontreAfricaine/SplashActivity.kt
+++ RencontreAfricaine/app/src/main/java/com/lnd/RencontreAfricaine/SplashActivity.kt
@@ -16,7 +16,7 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.lnd.RencontreAfricaine.R
 
-
+// feature branch
 class SplashActivity : AppCompatActivity() {
     val CURRENT_VERSION = 3
     val progressStep = 20
