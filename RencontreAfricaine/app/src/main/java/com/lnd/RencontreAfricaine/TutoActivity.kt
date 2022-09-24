92410d8ca5416d7d1fc382cdde7b0b9b9369cde2
Allassane <dickoallassane1997@gmail.com>
2022-09-24 21:08:10 +0000

    finish splachScreen

---
+++ RencontreAfricaine/app/src/main/java/com/lnd/RencontreAfricaine/TutoActivity.kt
@@ -0,0 +1,12 @@
+package com.lnd.RencontreAfricaine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TutoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tuto)
+        findView
+    }
+}
